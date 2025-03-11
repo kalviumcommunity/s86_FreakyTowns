f0d6713f368ff7962208d93b5a6eb7adf3c6459e
vaishnavi640 <vaishnavikalluri@gmail.com>
2025-03-11 11:17:07 +0530

    mongodb client success

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 // Import the Express module
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+app.use(express.json())
+require('dotenv').config()
 const PORT = 3000;
+
+
+mongoose.connect(process.env.DB_URL)
+.then(()=>{
+    console.log("mongodb is connected");
+})
+.catch((error)=>{
+    console.log("Failed to load",error);
+})
 
 // Root route
 app.get('/', (req, res) => {
